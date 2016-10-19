12ad0222d7b4f0e484bd5b29ede2048b61a8b1fc
GMD1AVeraGerritse <arev_97@live.nl>
2016-10-19 13:09:22 +0200

    start

---
+++ New Unity Project/Assets/scrips/Start.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Start : MonoBehaviour {
+    public Vector3 dir;
+    public Rigidbody bal;
+	
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        dir = collision.contacts[0].point;
+
+     
+            bal.AddForce(dir * -191);
+        
+    }
+}
