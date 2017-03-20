04b177b383dad4ab6eb65ea8011f1c788ee9eec3
GMD1AVeraGerritse <arev_97@live.nl>
2017-03-20 15:34:42 +0100

    p3

---
+++ p3/unity project/project 3 vluchtelingen/Assets/ConMan.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConMan : MonoBehaviour {
+    public static bool isTalking;
+    public Conversation con1;
+
+    void Start () {
+
+	}
+	
+
+	void Update () {
+		
+	}
+    public void Choice1()
+    {
+
+    }
+
+    public void Choice2()
+    {
+       
+        if (isTalking == false)
+        {
+            con1.antwoordCount = 1;
+            con1.npc = 1;
+        }
+        con1.NextStage();
+    }
+
+    public void Choice3()
+    {
+
+    }
+}
