38f9b0555c25bc4862e89f8b59e09e816754bd11
GMD1AVeraGerritse <arev_97@live.nl>
2017-03-21 15:46:38 +0100

    p3

--- p3/unity project/project 3 vluchtelingen/Assets/scripts/Movement/Move3rdPerson.cs
+++ p3/unity project/project 3 vluchtelingen/Assets/scripts/Movement/Move3rdPerson.cs
@@ -16,33 +16,46 @@ public class Move3rdPerson : MonoBehaviour {
     public float scrollSpeed;
     public Vector3 chara;
     public Vector3 cammera;
+    public Transform mid;
+    public float turnSpeed;
 
 	// Use this for initialization
 	void Start () {
-        chara = transform.position;
+        
         cammera = cam.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+        chara = transform.position;
         horizontal = Input.GetAxis("Mouse X") * mouseSen ;
         vertical = Input.GetAxis("Mouse Y") * mouseSen ;
         movVert = Input.GetAxis("Vertical") * movSpeed;
         movHor = Input.GetAxis("Horizontal") * movSpeed;
         scroll = Input.GetAxis("Mouse ScrollWheel");
 
+       
+        cam.transform.Rotate(-vertical* Time.deltaTime, 0, 0);
+        if(Input.GetButton("Vertical") == true)
+        {
+            mid.transform.rotation = Quaternion.Lerp(mid.rotation, myTransform.rotation, turnSpeed * Time.deltaTime);
+            myTransform.Rotate(0, horizontal * Time.deltaTime, 0);
+            transform.Translate(new Vector3(movHor, 0, movVert) * Time.deltaTime);
+        }
+        else if (Input.GetButton("Horizontal"))
+        {
+        mid.transform.rotation = Quaternion.Lerp(mid.rotation, myTransform.rotation, turnSpeed * Time.time);
         myTransform.Rotate (0,horizontal * Time.deltaTime,0);
         transform.Translate(new Vector3 (movHor,0, movVert) * Time.deltaTime);
-        cam.transform.Rotate(-vertical* Time.deltaTime, 0, 0);
+        }
+        mid.transform.Rotate(0, horizontal * Time.deltaTime, 0);
 
-        //cam.transform.Translate((transform.position - cam.transform.position).normalized * scrollSpeed * Time.deltaTime * scroll);
-
-    if(scroll > 0.0f)
+        if (scroll > 0.0f)
         {
             cam.transform.position = Vector3.MoveTowards(cam.transform.position, chara, scrollSpeed * Time.deltaTime);
         }
-    if (scroll < 0.0f)
+
+        if (scroll < 0.0f)
         {
             cam.transform.position = Vector3.MoveTowards(cam.transform.position, camPoint.transform.position, scrollSpeed *Time.deltaTime);
         }
---
+++ p3/unity project/project 3 vluchtelingen/Assets/scripts/Movement/TestLerp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLerp : MonoBehaviour {
+    public Transform start;
+    public Transform eind;
+    public float speed;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.rotation = Quaternion.Lerp(start.rotation, eind.rotation, speed *Time.time );
+		
+	}
+}
